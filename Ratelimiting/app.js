836b70fae03a9ef8f45d7d4370fb108be2ea773b
Carlos Garcia <carlosgarciag28@gmail.com>
2023-10-23 09:54:41 +0200

    added nodeJs

---
+++ Ratelimiting/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/static"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("", (req, res) => {
+  res.render("index.ejs", { error: null });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username != "devteam" || password != "letmein") {
+    return res.render("index.ejs", { error: "Invalid username or password" });
+  } else {
+    res.render("pwned.ejs");
+  }
+  res.render("index.ejs", { error: null });
+});
+
+app.use(function (req, res) {
+  res.status(404).render("404.ejs");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, "0.0.0.0", () =>
+  console.log(`Listening on port ${port}...!!!`)
+);
